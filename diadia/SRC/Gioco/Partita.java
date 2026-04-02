99540a7207e3bcfb8dbf64933fbade918368c9f8
stefa <stefa@10.25.70.74>
2026-04-02 16:31:24 +0200

    Creata la classe Labirinto

--- diadia/SRC/Gioco/Partita.java
+++ diadia/SRC/Gioco/Partita.java
@@ -14,57 +14,22 @@ public class Partita {
 
 	private Stanza stanzaCorrente;
 	private Stanza stanzaVincente;
+	private Labirinto labirinto;
 	private boolean finita;
 	private int cfu;
 	
 	/*DA CORREGERE Non ce bisogno di avere la responsabilità del labirinto, vado
 	 * a creare proprio la classe labirinto */
 	public Partita(){
-		creaStanze();
 		this.finita = false;
 		this.cfu = CFU_INIZIALI;
+		this.labirinto = new Labirinto();
 	}
 
-    /**
-     * Crea tutte le stanze e le porte di collegamento
-     */
-	//DA CORREGERE è PIU TIPICA DELLA PARTITA
-    private void creaStanze() {
-
-		/* crea gli attrezzi */
-    	Attrezzo lanterna = new Attrezzo("lanterna",3);
-		Attrezzo osso = new Attrezzo("osso",1);
-    	
-		/* crea stanze del labirinto */
-		Stanza atrio = new Stanza("Atrio");
-		Stanza aulaN11 = new Stanza("Aula N11");
-		Stanza aulaN10 = new Stanza("Aula N10");
-		Stanza laboratorio = new Stanza("Laboratorio Campus");
-		Stanza biblioteca = new Stanza("Biblioteca");
-		
-		/* collega le stanze */
-		atrio.impostaStanzaAdiacente("nord", biblioteca);
-		atrio.impostaStanzaAdiacente("est", aulaN11);
-		atrio.impostaStanzaAdiacente("sud", aulaN10);
-		atrio.impostaStanzaAdiacente("ovest", laboratorio);
-		aulaN11.impostaStanzaAdiacente("est", laboratorio);
-		aulaN11.impostaStanzaAdiacente("ovest", atrio);
-		aulaN10.impostaStanzaAdiacente("nord", atrio);
-		aulaN10.impostaStanzaAdiacente("est", aulaN11);
-		aulaN10.impostaStanzaAdiacente("ovest", laboratorio);
-		laboratorio.impostaStanzaAdiacente("est", atrio);
-		laboratorio.impostaStanzaAdiacente("ovest", aulaN11);
-		biblioteca.impostaStanzaAdiacente("sud", atrio);
-
-        /* pone gli attrezzi nelle stanze */
-		aulaN10.addAttrezzo(lanterna);
-		atrio.addAttrezzo(osso);
-
-		// il gioco comincia nell'atrio
-        stanzaCorrente = atrio;  
-		stanzaVincente = biblioteca;
+    public Labirinto getLabirinto() {
+    	return this.labirinto;
     }
-
+    
 	public Stanza getStanzaVincente() {
 		return stanzaVincente;
 	}
